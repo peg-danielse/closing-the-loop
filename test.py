add4323f8391e3b2614c87468d8ad2d5df17f54d
Paul Danielse <peg.danielse@gmail.com>
2025-03-24 12:27:02 +0100

    created a knowledge graph of performance anomalies and mitigation methods

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-# Use a pipeline as a high-level helper
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 import torch
 
@@ -8,8 +7,8 @@ torch.cuda.ipc_collect()
 torch.manual_seed(42)
 torch.cuda.manual_seed_all(42)
 
-
 model_path = "deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
+model_path = "NovaSky-AI/Sky-T1-7B-Zero"
 
 quantization_config = BitsAndBytesConfig(
     load_in_4bit=True,  # Enable 4-bit quantization
@@ -25,16 +24,47 @@ device = torch.device('cuda')
 model.to(device)
 
 
-input_texts = ['I want you to make a descision about what to do in this situation: I have 3 docker images and a load of 80\% cpu usage on server 1 and 1 docker image and a load of 20% cpu usage on server 2. do you 1: move images, 2: do nothing',
-               'I want you to think of a format to make a choice in conserning moving docker image from one server to another in order to distribute load'] #["When I grow up I will be a clouds engineer", 'what model are you?', 'can you think of a nickname for my wife: judy?']
+input_texts = ['I want you to make a descision about what to do in this situation: I have 3 docker images and a load of 80\% cpu usage on server 1 and 1 docker image and a load of 20% cpu usage on server 2. do you 1: move 1 image, 2: move 2 images, 3: do nothing', 
+               'please generate an example of a JSON structure',
+               'I want to ask you questions and get responses in a json format with the some integers detailing the answer to my question and some reasoning to why those where chosen. how can i best ask you to do so?',
+               '''{
+  "question": "What is the capital of France?",
+  "options": [
+    "Paris",
+    "London",
+    "Berlin",
+    "Rome"
+  ],
+  "correct_answer": "?",
+  "reasoning": "?"
+}
+''',
+'''
+can you copy this format and fill in the question marks for me? 
+\\boxed{
+  "question": "What is the capital of France?",
+  "options": [
+    "Paris",
+    "London",
+    "Berlin",
+    "Rome"
+  ],
+  "correct_answer": "?",
+  "reasoning": "?"
+}
+'''
+]
 
+'''En dan nog dit'''
 
-for input_text in input_texts:
-    inputs = tokenizer.apply_chat_template([{'role': 'user', 'content': "can you respond in this json format: { \'choice\': 0, \'reason\': \'\' }" },
-                                            {'role': 'user', 'content': input_text}], add_generation_prompt=True, return_tensors='pt', return_dict=True).to(device)
+print(tokenizer.chat_template)
 
-    outputs = model.generate(inputs["input_ids"], attention_mask=inputs["attention_mask"], pad_token_id=tokenizer.eos_token_id, max_length=500, temperature=0.7)
+# for input_text in input_texts:
+#     inputs = tokenizer.apply_chat_template([{'role': 'user', 'content': input_text }],
+#                                            add_generation_prompt=True, return_tensors='pt', return_dict=True).to(device)
 
-    print(tokenizer.decode(outputs[0], skip_special_tokens=True))
-    print("============= END ==============")
+#     outputs = model.generate(inputs["input_ids"], attention_mask=inputs["attention_mask"], pad_token_id=tokenizer.eos_token_id, do_sample=True, temperature=0.6)
+
+#     print(tokenizer.decode(outputs[0], skip_special_tokens=True))
+#     print("============================ END ===============================")
 
